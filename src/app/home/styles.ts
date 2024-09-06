7da8f4c5b19200175fe5b226fe2f3463e5bd40d6
Mirela Viana <mirelaviana.melo@gmail.com>
2024-09-06 11:27:20 -0300

    :sparkles: feat:  Desenvolvendo Barra de pesquisa

--- src/app/home/styles.ts
+++ src/app/home/styles.ts
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import { theme } from "@/theme/index";
+import { Input } from "../components/input";
 
 export const styles = StyleSheet.create({
     container: {
@@ -10,5 +11,10 @@ export const styles = StyleSheet.create({
         width: "100%",
         height: 132,
         backgroundColor: theme.colors.blue,
+        justifyContent: "flex-end",
+        paddingHorizontal: 24,
     },
+    Input: {
+        marginBottom: -27,
+    }
 })
